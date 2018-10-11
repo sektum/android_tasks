d893d91cf5289afd7caa52c7eb92a01388d75d27
Mykhailo Hrois <mykhailo_hrois@epam.com>
2018-10-11 13:06:01 +0300

    task4 begins

---
+++ task4/app/src/main/java/com/epam/mykhailo_hrois/task4/FirstActivity.java
@@ -0,0 +1,59 @@
+package com.epam.mykhailo_hrois.task4;
+
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
+
+public class FirstActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener {
+
+    Fragment1 fragment1 = Fragment1.newInstance(Color.CYAN);
+    Fragment2 fragment2 = new Fragment2();
+    Fragment3 fragment3 = new Fragment3();
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.first_container, fragment1)
+                .add(R.id.second_container, fragment2)
+                .addToBackStack(null)
+                .commit();
+        findViewById(R.id.second_container).setBackgroundColor(Color.LTGRAY);
+    }
+
+    @Override
+    public void onFragmentInteraction(int color) {
+        if(color == Color.CYAN) {
+            fragment1.setmColor(Color.MAGENTA);
+        }else {
+            fragment1.setmColor(Color.CYAN);
+        }
+        if(fragment2.getView() != null) {
+            fragment2.getView().setBackgroundColor(color);
+        }
+        if(fragment3.getView() != null) {
+            fragment3.getView().setBackgroundColor(color);
+        }
+    }
+
+    @Override
+    public void onPlaceFragments(boolean place) {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if(place){
+            fragmentTransaction.replace(R.id.second_container, fragment2);
+        }
+        else {
+            fragmentTransaction.replace(R.id.second_container, fragment3);
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
---
+++ task4/app/src/main/java/com/epam/mykhailo_hrois/task4/Fragment2.java
@@ -0,0 +1,22 @@
+package com.epam.mykhailo_hrois.task4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment2_layout, container, false);
+        return view;
+    }
+}
