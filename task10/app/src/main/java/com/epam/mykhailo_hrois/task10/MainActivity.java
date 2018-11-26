72dfdf512a86137fa56dbebfd4e6f7a293567d12
Mykhailo Hrois <mykhailo_hrois@epam.com>
2018-11-26 17:52:21 +0200

    task10 Room database added with db + dao + repository + viewModel

--- task10/app/src/main/java/com/epam/mykhailo_hrois/task10/MainActivity.java
+++ task10/app/src/main/java/com/epam/mykhailo_hrois/task10/MainActivity.java
@@ -1,13 +1,29 @@
 package com.epam.mykhailo_hrois.task10;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
