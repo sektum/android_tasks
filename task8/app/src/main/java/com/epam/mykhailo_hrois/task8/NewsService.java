801d630e91c0aac0102c69e177f894047a62785b
Mykhailo Hrois <mykhailo_hrois@epam.com>
2018-11-26 15:13:13 +0200

    imports fixed

--- task8/app/src/main/java/com/epam/mykhailo_hrois/task8/NewsService.java
+++ task8/app/src/main/java/com/epam/mykhailo_hrois/task8/NewsService.java
@@ -6,8 +6,6 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.format.DateFormat;
